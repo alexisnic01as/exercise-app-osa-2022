d41336d48fefff2782e6446ffae7efb41e651825
dannhxwan6 <103629433+dannhxwan6@users.noreply.github.com>
2022-07-25 23:29:03 -0500

    Added newWorkout button.

---
+++ client/src/components/newWorkout.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { Button, Dialog, DialogTitle, DialogContentText, DialogContent, DialogActions,
+    TextField, FormControl, Radio, FormLabel, RadioGroup, FormControlLabel } from "@mui/material";
+
+
+export default function NewWorkout() {
+
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [workoutType, setWorkoutType] = useState('Time-based');
+    const [workoutName, setWorkoutName] = useState('');
+    const [workoutAmount, setWorkoutAmount] = useState('');
+
+    const handleRadioChange = (event) => {
+        setWorkoutType(event.target.value);
+    };
+    
+    const handleClick = () => {
+        setDialogOpen(true);
+    }
+
+    const handleClose = () => {
+        setDialogOpen(false);
+    }
+
+    const handleFormSubmit = () => {
+        console.log(workoutName)
+        console.log(workoutAmount)
+    }
+
+
+    return (
+        <div>
+            <Button variant="contained" onClick = {handleClick}>Add a new workout!</Button>
+            <Dialog open = {dialogOpen} onClose={handleClose}>
+                <DialogTitle>Create a new workout!</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Share a workout for other people to see! Input the workout name and details then submit.
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Workout Name"
+                        fullWidth
+                        variant="standard"
+                        value={workoutName}
+                        onChange={(e) => setWorkoutName(e.target.value)}
+                    />
+                    <body><br></br></body>
+                    <FormControl>
+                        <FormLabel id="demo-controlled-radio-buttons-group">Workout Type</FormLabel>
+                        <RadioGroup
+                            aria-labelledby="demo-controlled-radio-buttons-group"
+                            name="controlled-radio-buttons-group"
+                            value={workoutType}
+                            onChange={handleRadioChange}
+                        >
+                            <FormControlLabel value="Amount of Time" control={<Radio />} label="Time-based" />
+                            <FormControlLabel value="Number of Repetitions" control={<Radio />} label="Rep-based" />
+                        </RadioGroup>
+                    </FormControl>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label={workoutType}
+                        fullWidth
+                        variant="standard"
+                        value={workoutAmount}
+                        onChange={(e) => setWorkoutAmount(e.target.value)}
+                    />
+                </DialogContent>
+                <Button onClick = {handleFormSubmit}> Submit Workout</Button>
+            </Dialog>
+        </div>
+    )
+
+}
